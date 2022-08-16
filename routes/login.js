0565cb7fa20baeada155a9507e86e38ae7387c11
Krishna <nayak100krishna@gmail.com>
2022-08-17 01:32:43 +0530

    redirect url set

--- routes/login.js
+++ routes/login.js
@@ -8,7 +8,7 @@ authUser.post("/api/login", (req, res) => {
   const code = req.body.code;
   console.log(code);
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
+    redirectUri: process.env.REDIRECT_URI_PRODUCTION,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
   });
@@ -31,7 +31,7 @@ authUser.post("/api/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
 
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
+    redirectUri: process.env.REDIRECT_URI_PRODUCTION,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     refreshToken,
